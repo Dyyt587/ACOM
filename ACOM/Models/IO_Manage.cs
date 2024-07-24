809bf1feb16cd51dc00523e66592af0ccd83f40d
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2024-07-24 22:16:37 +0800

    Create IO_Manage.cs

---
+++ ACOM/Models/IO_Manage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**
+ * 这个类用于管理IO输入输出
+ * 
+ * 
+ * 
+ * 
+ */
+namespace ACOM.Models
+{
+    class IO_Manage
+    {
+    }
+}
