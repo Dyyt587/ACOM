5d2b0ebc70ee3b6117a379a7051dd1d3fa155b9f
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2024-09-02 12:34:23 +0800

    t

--- ACOM/Views/MainCanvasPage.xaml.cs
+++ ACOM/Views/MainCanvasPage.xaml.cs
@@ -1,5 +1,22 @@
-// To learn more about WinUI, the WinUI project structure,
+﻿// To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
+
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using System.Collections.ObjectModel;
+using LiveChartsCore.Defaults;
+using Newtonsoft.Json.Linq;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 
 namespace ACOM.Views
 {
@@ -8,9 +25,85 @@ namespace ACOM.Views
     /// </summary>
     public sealed partial class MainCanvasPage : Page
     {
+        private readonly Random _random = new();
+        private readonly List<DateTimePoint> _values = new();
+        private static TimeSpan span = new TimeSpan(0, 0, 0, 0, 10);
+        private readonly DateTimeAxis _customAxis;// = new DateTimeAxis(span, Formatter);
+        public ObservableCollection<ISeries> SeriesDates { get; set; } = new ObservableCollection<ISeries>();
+        public Axis[] XAxesDates { get; set; }
+
+        public object SyncDates { get; } = new object();
+        public bool IsReading { get; set; } = true;
+
+        private async Task ReadData()
+        {
+            // to keep this sample simple, we run the next infinite loop 
+            // in a real application you should stop the loop/task when the view is disposed 
+            while (IsReading)
+            {
+                await Task.Delay(100);
+
+                // Because we are updating the chart from a different thread 
+                // we need to use a lock to access the chart data. 
+                // this is not necessary if your changes are made in the UI thread. 
+                lock (SyncDates)
+                {
+                    _values.Add(new DateTimePoint(DateTime.Now, _random.Next(0, 10)));
+                    if (_values.Count > 250) _values.RemoveAt(0);
+
+                    // we need to update the separators every time we add a new point 
+                    _customAxis.CustomSeparators = GetSeparators();
+                }
+            }
+        }
+        private double[] GetSeparators()
+        {
+            var now = DateTime.Now;
+
+            return new double[]
+            {
+            now.AddSeconds(-25).Ticks,
+            now.AddSeconds(-20).Ticks,
+            now.AddSeconds(-15).Ticks,
+            now.AddSeconds(-10).Ticks,
+            now.AddSeconds(-5).Ticks,
+            now.Ticks
+            };
+        }
+        private static string Formatter(DateTime date)
+        {
+            var secsAgo = (DateTime.Now - date).TotalSeconds;
+
+            return secsAgo < 1
+                ? "now"
+                : $"{secsAgo:N0}s ago";
+        }
         public MainCanvasPage()
         {
+            SeriesDates = new ObservableCollection<ISeries>
+        {
+            new LineSeries<DateTimePoint>
+            {
+                Values = _values,
+                Fill = null,
+                GeometryFill = null,
+                GeometryStroke = null
+            }
+        };
+
+        _customAxis = new DateTimeAxis(TimeSpan.FromSeconds(1), Formatter)
+        {
+            CustomSeparators = GetSeparators(),
+            AnimationsSpeed = TimeSpan.FromMilliseconds(0),
+            SeparatorsPaint = new SolidColorPaint(SKColors.Black.WithAlpha(100))
+        };
+
+        XAxesDates = new Axis[] { _customAxis };
+        _ = ReadData();
+
             this.InitializeComponent();
+            chart1.XAxes = XAxesDates;
+
         }
     }
 }
